70f02283de73c7e55c637f40e0ac2b6e3b0e6756
Delta student <shreya0427singh@gmail.com>
2026-09-05 20:02:54 +0530

    Add validation middleware for listings and refactor create/update routes

--- app.js
+++ app.js
@@ -37,6 +37,16 @@ app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
 
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
 
 // Index Route
 app.get(
@@ -71,12 +81,9 @@ app.get(
 // Create Route
 app.post(
     "/listings",
+    validateListing,
     wrapAsync(async (req, res, next) => {
-        let result = listingSchema.validate(req.body);
-        console.log(result);
-        if (result.error) {
-           throw new ExpressError(400, result.error);
-        }
+        
 
         const newListing = new Listing(req.body.listing);
         await newListing.save();
@@ -100,10 +107,9 @@ app.get(
 // Update Route
 app.put(
   "/listings/:id",
+  validateListing,
   wrapAsync(async (req, res) => {
-    if (!req.body.listing) {
-      throw new ExpressError(400, "Send valid data for listing");
-    }
+    
     let { id } = req.params;
 
     await Listing.findByIdAndUpdate(
